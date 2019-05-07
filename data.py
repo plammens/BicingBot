0ec69e426c1bcab9bbb41f0ef58446ebbddfa4c5
Paolo Lammens <lammenspaolo@gmail.com>
2019-05-07 16:22:47 +0200

    feat: Station class (graph node)

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
+import networkx as nx
 import pandas as pd
 
 URL = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
 
 json_data: pd.DataFrame
 stations: pd.DataFrame
+graph: nx.Graph
 
 
 def fetch_data():
@@ -12,3 +14,12 @@ def fetch_data():
     json_data = pd.read_json(URL)
     stations = pd.DataFrame.from_records(data=json_data.data.stations, index='station_id')
 
+
+class Station:
+    id: int
+
+    def __init__(self, index: int):
+        self.id = index
+
+    def get_data(self):
+        return stations[self.id]

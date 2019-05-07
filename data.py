ca72272f71afcbeb6cea93d902ef5b6fd4e241ef
Paolo Lammens <lammenspaolo@gmail.com>
2019-05-07 14:44:03 +0200

    canvi superestelar

--- data.py
+++ data.py
@@ -10,4 +10,7 @@ def fetch_data():
     global json_data, stations
 
     json_data = pd.read_json(URL)
-    stations = pd.DataFrame.from_records(data=json_data.data.stations, index='station_id')
+    stations = pd.DataFrame.from_reco/rds(data=json_data.data.stations, index='station_id')
+
+
+kofprjfipow

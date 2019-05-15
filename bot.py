1c90a73aff57d6832ed7b81129e6c58995da0f9a
Paolo Lammens <lammenspaolo@gmail.com>
2019-05-15 12:42:39 +0200

    refactor: switch to context-based callbacks

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import telegram.ext as tge
 with open('token.txt') as token_file:
     TOKEN: str = token_file.read().strip()
 
-UPDATER = tge.Updater(token=TOKEN)
+UPDATER = tge.Updater(token=TOKEN, use_context=True)
 DISPATCHER = UPDATER.dispatcher
 BOT = UPDATER.bot
 
@@ -23,71 +23,72 @@ def cmdhandler(command: str = None, **handler_kwargs) -> callable:
 
     :param command: name of bot command to add a handler for
     :param handler_kwargs: additional keyword arguments for the
-                           creation of the command handler
+                           creation of the command handler (these will be passed
+                           to ``telegram.ext.dispatcher.add_handler``)
     :return: the decorated function, unchanged
     """
 
     # Actual decorator
-    def decorator(callback: callable) -> tge.CommandHandler:
+    def decorator(callback: callable) -> callable:
         nonlocal command
         command = command or callback.__name__
 
-        def decorated(bot, update, *args, **kwargs):
+        def decorated(update: tg.Update, context: tge.CallbackContext, *args, **kwargs):
             try:
-                return callback(bot, update, *args, **kwargs)
+                return callback(update, context, *args, **kwargs)
             except Exception as e:
                 text = ERROR_TXT + "\n\n`{}`".format(format_exception_only(type(e), e)[0])
                 update.message.reply_markdown(text)
 
         handler = tge.CommandHandler(command, decorated, **handler_kwargs)
         DISPATCHER.add_handler(handler)
-        return callback
+        return decorated
 
     return decorator
 
 
 @cmdhandler()
-def start(bot: tg.Bot, update: tg.Update):
+def start(update: tg.Update, context: tge.CallbackContext):
     update.message.reply_markdown(START_TXT)
 
 
 @cmdhandler()
-def help(bot: tg.Bot, update: tg.Update):
+def help(update: tg.Update, context: tge.CallbackContext):
     update.message.reply_markdown(HELP_TXT)
 
 
 @cmdhandler()
-def authors(bot: tg.Bot, update: tg.Update):
+def authors(update: tg.Update, context: tge.CallbackContext):
     update.message.reply_markdown(AUTHORS_TXT)
 
 
 @cmdhandler()
-def graph(bot: tg.Bot, update: tg.Update):
+def graph(update: tg.Update, context: tge.CallbackContext):
     raise NotImplementedError
 
 
 @cmdhandler()
-def nodes(bot: tg.Bot, update: tg.Update):
+def nodes(update: tg.Update, context: tge.CallbackContext):
     raise NotImplementedError
 
 
 @cmdhandler()
-def edges(bot: tg.Bot, update: tg.Update):
+def edges(update: tg.Update, context: tge.CallbackContext):
     raise NotImplementedError
 
 
 @cmdhandler()
-def components(bot: tg.Bot, update: tg.Update):
+def components(update: tg.Update, context: tge.CallbackContext):
     raise NotImplementedError
 
 
 @cmdhandler()
-def route(bot: tg.Bot, update: tg.Update):
+def route(update: tg.Update, context: tge.CallbackContext):
     raise NotImplementedError
 
 
 @cmdhandler()
-def plotgraph(bot: tg.Bot, update: tg.Update):
+def plotgraph(update: tg.Update, context: tge.CallbackContext):
     raise NotImplementedError
 
 
@@ -97,8 +98,6 @@ def start_bot():
                         level=logging.INFO)
 
     UPDATER.start_polling()
-
-
 
 
 START_TXT: str
